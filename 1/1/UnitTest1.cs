d0cac2cb1c9cf601974964fc7feb2231c7619173
aipov <aipov@users.noreply.github.com>
2018-08-08 11:44:04 +0300

    создание

---
+++ 1/1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("https://site2017.ingos.ru/travel/abroad/calc/");
+            driver.Manage().Window.FullScreen();
+            driver.FindElement(By.XPath("//span[@class='multiselect__empty ng-scope']")).Click();
+            driver.Quit();
+        }
+
+    }
+}
